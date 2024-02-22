cf7973baae223ab9e3538659f4be173a4df8053b
ManuelDeLaEranueva <manueranueva@gmail.com>
2024-02-22 14:37:59 +0100

    correcciones añadidas #286

--- staff/manuel-eranueva/strings/concate copy.js
+++ staff/manuel-eranueva/strings/concate copy.js
@@ -1,17 +1,19 @@
 delete String.prototype.concat
 
-function concat(string1, string2) {
-    var string3 = ''
-    if (string1 == Number && string2 == Number) {
-        string3 = string1.toString() + string2
-    } else {
-        string3 = string1 + string2
-    }
-    return string3
-}
+function concat(...string) {
+    var fullString = ''
+    for (i = 0; i < string.length; i++) {
+        fullString += string[i]
 
+    }
+    return fullString
+}
 var string1 = 555
 var string2 = 5552
+var string3 = ' Hola '
+var string4 = 'buenos '
+var string5 = 'días. '
+var string6 = true
 
-var string3 = concat(string1, string2);
-console.log(string3);
+var fullString = concat(string1, string2, ' ', 123, 45, string3, string4, string5, string6, ' taluego');
+console.log(fullString);
