16723aec8e5b40f9fa1d7dfa36fac2ac51c3a3e9
ManuelDeLaEranueva <manueranueva@gmail.com>
2024-04-03 10:53:26 +0200

    added lastest versions #360

---
+++ staff/manuel-eranueva/isdigram/api/index.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import logic from './logic/index.mjs'
+
+const api = express()
+
+const jsonBodyParser = express.json()
+
+api.post('/users', jsonBodyParser, (req, res) => {
+    try {
+        const { name, birthdate, email, username, password } = req.body
+
+        logic.registerUser(name, birthdate, email, username, password, error => {
+            if (error) {
+                res.status(400).json({ error: error.constructor.name, message: error.message })
+
+                return
+            }
+
+            res.status(201).send()
+        })
+    } catch (error) {
+        res.status(400).json({ error: error.constructor.name, message: error.message })
+    }
+
+})
+api.listen(800, () => console.log('API listening on port 8080'))
