d6be51dfc350262c9d766a79c8985f77f61472f4
ManuelDeLaEranueva <manueranueva@gmail.com>
2024-05-07 16:09:20 +0200

    WIP #466

--- staff/manuel-eranueva/project/app/src/pages/Home.jsx
+++ staff/manuel-eranueva/project/app/src/pages/Home.jsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react'
+
 import logic from '../logic'
+
+
 import BookList from '../components/BookList'
 import Popup from '../components/Popup'
 import CreateCard from '../components/CreateCard'
@@ -7,6 +10,7 @@ import CardList from '../components/CardList'
 
 function Home({ onUserLoggedOut }) {
     const [user, setUser] = useState(null)
+    const [view, setView] = useState(null)
     const [books, setBooks] = useState([])
     const [selectedBook, setSelectedBook] = useState(null);
     const [popupOpen, setPopupOpen] = useState(false)
@@ -23,7 +27,14 @@ function Home({ onUserLoggedOut }) {
             .catch(error => console.error('Failed to retrieve books:', error))
     }, [])
 
+    const clearView = () => setView(null)
+    const handleCreateCardCancelClick = () => clearView()
 
+    const handleCardCreated = () => {
+        clearView()
+    }
+
+    const handleCreateCardClick = () => setView('create-card')
 
     const handleLogoutClick = () => {
         try {
@@ -66,9 +77,14 @@ function Home({ onUserLoggedOut }) {
                 </nav>
             </header>
             <main>
+                {view === 'create-card' && <CreateCard onCancelClick={handleCreateCardCancelClick} onCardCreated={handleCardCreated} />}
                 <BookList onBookSelect={handleSelectedBook} books={books} />
                 {popupOpen && selectedBook && <Popup book={selectedBook} onClose={handleClosePopup} onCreateCard={handleCreateCard} />}
             </main>
+
+            <footer className="fixed bottom-0 w-full h-[50px] flex justify-center items-center p-[10px] box-border bg-white">
+                <button onClick={handleCreateCardClick}>➕</button>
+            </footer>
         </>
     )
 }
